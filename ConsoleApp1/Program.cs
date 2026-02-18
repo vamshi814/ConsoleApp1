c454be48bcd6c5c92871b6991e3266263f3d547f
vamshi814 <vamshi814.s@gmail.com>
2026-02-18 17:35:53 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hi Main method!!!");
+        Console.WriteLine("Main execution//////.........");
+
+    }
+}
+
