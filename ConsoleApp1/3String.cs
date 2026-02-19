3bb37adbc3776d73a40d2e243c00e7c02eb0f76c
vamshi814 <vamshi814.s@gmail.com>
2026-02-19 11:35:53 +0530

    Type ConversionsVar Dynamic added

---
+++ ConsoleApp1/3String.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Security.Cryptography;
+
+
+class String
+{
+    static void Main()
+    {
+        /*
+         * String is one kind of class in C#
+         * Contains members like
+         * Property:describes about characteristcs (name ,id, ..)
+         * Method: nothing but actions performed(cooking, driving, earning ..)
+         * 
+         * */
+        string s = "this is a string";
+        var textLength = s.Length;
+        Console.WriteLine($"Text Length is: {textLength}");
+
+        string upperS = s.ToUpper();
+        Console.WriteLine($"Upper Text is: {upperS}");
+
+        string s1 = "      trimmmmm     ";
+        Console.WriteLine(s1.Trim());
+        string s2 = "*********trtrrrtrtrtr************";
+        Console.WriteLine("Removed stars from starting and ending: " + s2.Trim('*'));
+        string s3 = "demon";
+        Console.WriteLine(s3 +" pad left " + s3.PadLeft(10,'~')+"\n-------------------------------");
+
+
+        //string comparison
+
+        string password = "Hello123";
+        bool isEqual = password.Equals("HelloWorld");
+        Console.WriteLine("bool value is: " + isEqual);
+
+        string s5 = "the quick brown fox jumps over a lazy dog";
+        string substring5 = s5.Substring(4);
+        Console.WriteLine("substring from 4: " + substring5);
+        string substring54 = s5.Substring(4, 5);
+        Console.WriteLine("Substring from 4 to length 5: " + substring54);
+        string stringReplaced = s5.Replace("brown", "black");
+        Console.WriteLine("String Replaced: " + stringReplaced);
+        Console.WriteLine("String format----------------------");
+        int quantity = 5;
+        double price = 1800.75;
+        string rcpt = string.Format("You ordered {0} items for total price {1}", quantity, price);
+        Console.WriteLine(rcpt);
+
+    }
+
+}
+
---
+++ ConsoleApp1/4TypeConversions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+class DataTypeConversions
+{
+    static void Main()
+    {
+        //Implicit conversion placing small type in large type
+        byte n = 10;
+        int n1 = n;
+        Console.WriteLine($"Large container: {n1}");
+
+        //Explicit conversion small type holding large type
+        //errors might occur,data loss might occurs
+        int employees = 256;
+        byte employeesByte = (byte)employees;//suppose if 256 -> 0 or (number % 256)
+        Console.WriteLine("Lower holder : " + employeesByte);
+
+
+        double price = 87.877;
+        int Iprice = (int)price;
+        Console.WriteLine("(int)Iprice: " + Iprice);
+        string stringValue = Iprice.ToString();
+        Console.WriteLine("StringVlaue-- .ToString()------ used  : "+stringValue);
+
+        string s6 = "89";
+        int Ivalue = int.Parse(s6);
+
+        Console.WriteLine("IVale int.Parse(\"89\")======"+Ivalue);
+        //boxing & unboxing
+        Console.WriteLine("Boxing & Unboxing: " +
+            "\nprocess of converting value type to reference type(object)." +
+            "\nUnboxing is reverse boxed value to original (through Explicit)possible.");
+        int n11= 44;
+        object obj = n11;//boxing
+        Console.WriteLine("boxing obj :   " + obj + " " + obj.GetType());
+        object obj1 = 333;
+        int n22 = (int)obj1;
+        Console.WriteLine("unboxing int :   " + n22 + " " + n22.GetType());
+
+
+    }
+}
