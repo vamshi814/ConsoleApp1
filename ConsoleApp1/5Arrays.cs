3702286457b3ffe9a9d50f41a27df6c88b34eafc
vamshi814 <vamshi814.s@gmail.com>
2026-02-20 16:14:36 +0530

    Arrays & ReadLine

---
+++ ConsoleApp1/5Arrays.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Arrays
+{
+    static void Main()
+    {
+        //Arrays. : container which holds collection of values of same datatype
+
+
+        Console.WriteLine("Arrays::");
+        int[] array1 = new int[] { 1, 2, 3 };
+        string[] array2 = { "maths", "ps", "cs" };
+        Console.WriteLine($"0th index of array1:{array1[0]}");
+        Console.WriteLine($"Array1 : {array1}");
+        int j = 1;
+        foreach (int val in array1)
+        {            
+            Console.WriteLine($"Value of {j++} is : {val}");
+        }
+        for(int i = 0; i < array2.Length; i++)
+        {
+            Console.WriteLine(i + " " + array2[i]);
+        }
+    }
+}
---
+++ ConsoleApp1/6ReadLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class ReadLine
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name : ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter ID: ");
+        int Id = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Name is : " + name + " Id is : " + Id);
+        Console.WriteLine(string.Format("\nHI {0} Confirm your ID {1}", name, Id));
+        
+    }
+}
